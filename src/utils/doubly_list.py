ca8112f689698531aad634d0fc38baa0aecc3fee
Alex L <alexlitvak90@gmail.com>
2025-02-11 23:07:52 +0200

    add tests for doubly-linked list

--- src/utils/doubly_list.py
+++ src/utils/doubly_list.py
@@ -1,10 +1,9 @@
 """Implementation of a doubly linked list"""
-#from dataclasses import dataclass, field
-from typing import Any, Optional, TypeVar
+from typing import Optional, TypeVar, Union
 
 from pydantic import BaseModel
 
-T = TypeVar("T")
+T = TypeVar(name="T", bound=Union[int, str])
 
 class Node(BaseModel):
     """Doubly-linked node"""
@@ -25,39 +24,50 @@ class DLL(BaseModel):
         return self.len == 0
 
 
-    def __add_first__(self, new: Node) -> None:
-        new.next = None
-        new.prev = None
-        self.head = new
-        self.tail = new
+    def __add_first__(self, value: T) -> Node:
+        node = Node(value=value)
+        node.next = None
+        node.prev = None
+        self.head = node
+        self.tail = node
         self.len = 1
+        return node
 
 
-    def push_head(self, node: Node) -> None:
+    def push_head(self, value: T) -> Node:
         if self.__is_first__():
-            return self.__add_first__(node)
+            return self.__add_first__(value)
 
-        tmp = self.head
-        tmp.prev = node
-        node.next = tmp
+        node = Node(value=value)
+        top = self.head
+        top.prev = node
+        node.next = top
         node.prev = None
         self.head = node
         self.len += 1
+        return node
 
 
-    def push_tail(self, new: Node) -> None:
+    def push_tail(self, value: T) -> Node:
         if self.__is_first__():
-            return self.__add_first__(new)
+            return self.__add_first__(value)
 
-        tmp = self.head
-        tmp.prev = new
-        new.next = tmp
-        new.prev = None
-        self.head = new
+        node = Node(value=value)
+        btm = self.tail
+        btm.next = node
+        node.prev = btm
+        node.next = None
+        self.tail = node
         self.len += 1
+        return node
 
 
     def remove(self, node: Node) -> None:
+        if ((node.prev is None or node.next is None)
+                and self.head.value != node.value
+                and self.tail.value != node.value):
+            return
+
         nex: Optional[Node] = node.next
         prev: Optional[Node] = node.prev
         if prev is not None and nex is not None:
@@ -77,4 +87,6 @@ class DLL(BaseModel):
             self.head = None
             self.tail = None
 
+        node.next = None
+        node.prev = None
         self.len -= 1
--- src/utils/test_doubly_list.py
+++ src/utils/test_doubly_list.py
@@ -9,24 +9,47 @@ class TestDoublyList(TestCase):
 
     def test_push_head(self):
         list1 = DLL(size=128)
-        node = Node(value=1)
-        list1.push_head(node)
+        node = list1.push_head(value=111)
+        self.assertNotEqual(None, node)
+        self.assertEqual(111, node.value)
         self.assertEqual(1, list1.len)
 
     def test_push_tail(self):
         list1 = DLL(size=128)
-        node = Node(value=1)
-        list1.push_head(node)
+        node = list1.push_head(111)
+        self.assertNotEqual(None, node)
+        self.assertEqual(111, node.value)
         self.assertEqual(1, list1.len)
 
     def test_remove(self):
-        list1 = DLL(size=128)
-        nodes = [Node(value=1), Node(value=2), Node(value=3), Node(value=4)]
-        for node in nodes:
-            list1.push_head(node)
-
+        list1: DLL = DLL(size=128)
+        created: [Node] = [list1.push_head((n+1) * 111) for n in range(4)]
         self.assertEqual(4, list1.len)
-        list1.remove(nodes[1])
+        self.assertEqual(4, len(created))
+        list1.remove(created[1])
+        list1.remove(created[2])
+        list1.remove(created[0])
+        self.assertEqual(1, list1.len)
+        self.assertEqual(444, list1.head.value)
+        self.assertEqual(444, list1.tail.value)
+
+    def test_remove_str(self):
+        list1: DLL = DLL(size=128)
+        created: [Node] = [list1.push_head((n + 1) * "hey ") for n in range(4)]
+        self.assertEqual(4, list1.len)
+        self.assertEqual(4, len(created))
+        list1.remove(created[2])
+        list1.remove(created[0])
+        list1.remove(created[3])
+        self.assertEqual(1, list1.len)
+        self.assertEqual("hey hey ", list1.head.value)
+        self.assertEqual("hey hey ", list1.tail.value)
+
+    def test_remove_missing(self):
+        list1: DLL = DLL(size=128)
+        created: [Node] = [list1.push_head((n + 1) * 111) for n in range(4)]
+        list1.remove(created[1])
+        list1.remove(created[1])
         self.assertEqual(3, list1.len)
 
 if __name__ == '__main__':
